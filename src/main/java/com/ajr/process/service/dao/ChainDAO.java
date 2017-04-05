229928ea423a33d2aae8dd23cc54a9832baa0280
aramalhosa <aramalhosa@gmail.com>
2017-04-05 09:43:11 +0100

    Novos desenvolvimento de 1 de abril

--- src/main/java/com/ajr/process/service/dao/ChainDAO.java
+++ src/main/java/com/ajr/process/service/dao/ChainDAO.java
@@ -7,22 +7,42 @@ import com.ajr.process.service.entity.ChainProject;
 import com.ajr.process.service.entity.ComponentRelation;
 import com.ajr.process.service.exceptions.AttributeAlreadyExistsException;
 
-
 public interface ChainDAO {
-	
-	public void insertProject(String project, String chainProject, String description);
-	public void insertProjectComponent(String project, String attribute, String description) throws AttributeAlreadyExistsException;
+
+	public void insertProject(String project, String chainProject,
+			String description);
+
+	public void insertProjectComponent(String project, String attribute,
+			String description) throws AttributeAlreadyExistsException;
+
 	public void insertComponentsRelation(int compId1, int compId2);
+
 	public void removeProjectComponents(int idProject);
-	public void updateSelecedProjectComponent(String project, int projChain, int component);
+
+	public void updateSelecedProjectComponent(String project, int projChain,
+			int component);
+
+	public void updateComponentRelations(int componentId,
+			List<Integer> relations);
+
 	public List<ChainProject> retrieveProjects(String project);
+
 	public List<ChainProjComponent> retrieveChainProjectComponents(int chainProj);
-	public List<ChainProjComponent> retrieveSelectedChainProjectComponents(String project);
+
+	public List<ChainProjComponent> retrieveSelectedChainProjectComponents(
+			String project);
+
 	public ChainProject retrieveProject(int IdProject);
+
 	public ChainProject retrieveProjectSelected(String project);
+
 	public ChainProjComponent retrieveComponent(int componentId);
-	public ChainProjComponent retrieveSelectedComponentFromSelectedProject(String project);
+
+	public ChainProjComponent retrieveSelectedComponentFromSelectedProject(
+			String project);
+
 	public List<Object[]> retrieveComponentRelations(int idComp);
+
 	public List<Object[]> retrieveRelations(int componentId);
 
 }
